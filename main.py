948d8c8c11d4bf92a8e25641a20dba9a43821ee7
andre williamson <AndreWillimsonMN@gmail.com>
2020-12-06 16:57:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this lin
+stuff = int(two_digit_number[0])
+stuff2 = int(two_digit_number[1])
+print(stuff + stuff2)
+
